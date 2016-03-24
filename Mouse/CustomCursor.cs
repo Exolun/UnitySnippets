9efdd7b68efbcf97bf5cdcbc64f297af8b9fa5a6
Michael Smith <mike@mikelikescoding.com>
2016-03-23 21:35:18 -0700

    Added snippet for custom mouse pointer.

---
+++ Mouse/CustomCursor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Provides a custom game cursor
+/// </summary>
+public class CustomCursor : MonoBehaviour {
+    /// <summary>
+    /// Texture to use for the pointer
+    /// </summary>
+    public Texture2D Texture;
+
+    /// <summary>
+    /// Position in the texture where the click event should occur.
+    /// </summary>
+    public Vector2 Hotspot;
+    
+	void Start () {
+        Cursor.SetCursor(this.Texture, this.Hotspot, CursorMode.ForceSoftware);
+        Cursor.visible = true;
+    }
+}

52228a13ed0a8dbb5b7a6841258aef502b7c8ac0
Michael Smith <mike@mikelikescoding.com>
2016-03-25 22:31:39 -0700

    Added SelectableUnit for selection code coming up

---
+++ Selection/SelectableUnit.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// A Unit that's selectable
+/// 
+/// Requires an object assigned to serve as the selection highlighting. Recommend adding a child object
+/// to whatever object your unit is with a halo or selection sprite for this class to toggle on/off to visually
+/// indicate selection.
+/// </summary>
+public class SelectableUnit : MonoBehaviour {
+    private bool isSelected = false;
+
+    /// <summary>
+    /// Object to toggle on/off for highlighting
+    /// </summary>
+    public GameObject SelectionHighlighting;
+
+    void Start()
+    {
+        this.SelectionHighlighting.SetActive(false);
+    }
+
+    public bool IsSelected() {
+        return this.isSelected;
+    }
+
+    public void Select() {
+        this.isSelected = true;
+        this.SelectionHighlighting.SetActive(true);
+    }
+
+    public void Deselect() {
+        this.isSelected = false;
+        this.SelectionHighlighting.SetActive(false);
+    }
+}

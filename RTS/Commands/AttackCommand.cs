f69a6676cb2d5389a6fd160f5dda5d10d4310c4d
Michael Smith <mike@mikelikescoding.com>
2016-03-28 23:19:47 -0700

    Stubbed out Attack/RotateTurretCommand

---
+++ RTS/Commands/AttackCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Commands
+{
+    public class AttackCommand : ICommand
+    {
+        private GameObject unit;
+        private Func<Vector3> attackTargetGetter;
+        private RotateTurretCommand rotTurretCmd;
+
+        public AttackCommand(GameObject unit, Func<Vector3> attackTargetGetter)
+        {
+            this.unit = unit;
+            this.attackTargetGetter = attackTargetGetter;
+
+            this.rotTurretCmd = new RotateTurretCommand(unit, attackTargetGetter);
+        }
+
+        public void Do()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsComplete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RTS/Commands/RotateTurretCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Commands
+{
+    public class RotateTurretCommand : ICommand
+    {
+        private GameObject unit;
+        private Func<Vector3> attackTargetGetter;
+        private GameObject turret;
+
+        public RotateTurretCommand(GameObject unit, Func<Vector3> attackTargetGetter)
+        {
+            this.unit = unit;
+            this.attackTargetGetter = attackTargetGetter;
+            this.turret = unit.transform.FindChild("Turret").gameObject;
+        }
+
+        public void Do()
+        {
+
+
+            throw new NotImplementedException();
+        }
+
+        public bool IsComplete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

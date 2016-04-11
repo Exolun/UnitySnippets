8e1eb8d2de8d9e96d5198c48771ba733da0f6638
Michael Smith <mike@mikelikescoding.com>
2016-04-10 20:57:54 -0700

    + Added some scripts from a pong game I made. (BallController contains most of the game)

---
+++ Pong/BallController.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+	public int Speed = 150;
+	public GameObject RestartPanel;
+
+	void Start () {
+		var rigidBody = this.gameObject.GetComponent<Rigidbody2D>();
+		rigidBody.AddForce (new Vector2 (this.Speed, this.Speed));
+	}
+	
+	void Restart()
+	{
+		var scoreLabel2 = GameObject.FindGameObjectWithTag("P2Score");
+		var text = scoreLabel2.GetComponentInChildren<UnityEngine.UI.Text>();
+		text.text = "0";
+
+		var scoreLabel1 = GameObject.FindGameObjectWithTag("P1Score");
+		text = scoreLabel1.GetComponentInChildren<UnityEngine.UI.Text>();
+		text.text = "0";
+
+		this.gameObject.transform.position = new Vector3(-3.0f, -1.0f, 0);
+		var rigidBody = this.gameObject.GetComponent<Rigidbody2D>();
+		rigidBody.velocity = new Vector2();
+		rigidBody.AddForce (new Vector2 (this.Speed, this.Speed));
+		this.RestartPanel.SetActive (false);
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.CompareTag ("Scoring Zone 1")) {
+			this.gameObject.transform.position = new Vector3(-3.0f, -1.0f, 0);				
+			
+			var scoreLabel2 = GameObject.FindGameObjectWithTag("P2Score");
+			var text = scoreLabel2.GetComponentInChildren<UnityEngine.UI.Text>();
+			text.text = (int.Parse(text.text) + 1).ToString();
+
+			var rigidBody = this.gameObject.GetComponent<Rigidbody2D>();
+			rigidBody.velocity = new Vector2();
+
+			if(int.Parse(text.text) > 4){
+				this.RestartPanel.SetActive(true);
+			}
+			else{
+				rigidBody.AddForce (new Vector2 (this.Speed, this.Speed));
+			}
+		} 
+		else if (other.gameObject.CompareTag ("Scoring Zone 2")) {
+			this.gameObject.transform.position = new Vector3(3.0f, 1.0f, 0);
+			var scoreLabel1 = GameObject.FindGameObjectWithTag("P1Score");
+			var text = scoreLabel1.GetComponentInChildren<UnityEngine.UI.Text>();
+			text.text = (int.Parse(text.text) + 1).ToString();
+
+			var rigidBody = this.gameObject.GetComponent<Rigidbody2D>();
+			rigidBody.velocity = new Vector2();
+
+			if(int.Parse(text.text) > 4){
+				this.RestartPanel.SetActive(true);
+			}
+			else{
+				rigidBody.AddForce (new Vector2 (-this.Speed, -this.Speed));
+			}
+		}
+	}
+
+	void OnCollisionExit2D (Collision2D other){
+		var rigidBody = this.gameObject.GetComponent<Rigidbody2D>();
+		rigidBody.velocity *= 1.05f;
+		var audio = this.gameObject.GetComponent<AudioSource> ();
+		audio.Play();
+
+		var particles = GameObject.FindWithTag ("Particles");
+		particles.transform.position = this.transform.position;
+		particles.GetComponent<ParticleSystem> ().Play();
+	}
+}
---
+++ Pong/PaddleController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleController : MonoBehaviour {
+	public KeyCode UpKey = KeyCode.W;
+	public KeyCode DownKey = KeyCode.S;
+	public float MovementSpeed = .1f;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var transform = this.gameObject.transform;
+
+		if (Input.GetKey (this.UpKey)) {
+			var upMovement = new Vector3(0, this.MovementSpeed, 0);
+			transform.position =  transform.position + upMovement;		
+		}
+		else if (Input.GetKey (this.DownKey)) {		
+			var downMovement = new Vector3(0, -this.MovementSpeed, 0);
+			transform.position =  transform.position + downMovement;
+		}
+	}
+}
